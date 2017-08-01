e4dbf6d2cff2f924658bd19da5a81042a509e365
simplon <costanzo.isabel@gmail.com>
2017-08-01 13:41:58 +0200

    exercice fait

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 	//votre code
+var count = 0 ;
+var array = [ "coucou", "pourquoi tu reviens", "au revoir"];
 
+$(".cake-is-a-lie").click(function(){
+	count++;
+	$(".counter").text(count);
+
+	var random= Math.random() * array.length;
+	var random= parseInt(random);
+
+	if(count%20===0){
+		alert(array[random]);
+}
+
+
+
+  })
 
 });
